cb7b0a54327b02663f730093ffe8e743f8c19ace
JEMinaya <jmartinezminaya3.cf@iesesteveterradas.cat>
2024-11-19 17:22:07 +0100

    Add files via upload

---
+++ activitat6.1.ps1
@@ -0,0 +1,15 @@
+﻿$NombreCarpeta = Read-Host ("Escribe el nombre del directorio a buscar")
+
+if (-Not $NombreCarpeta) {
+    Write-Host ("Se debe dar el nombre del directorio a buscar.")
+    exit
+}
+
+$RutaCompleta = Get-ChildItem -Recurse -Depth 2 -ErrorAction SilentlyContinue -Path "C:\" | Where-Object { $_.Name -like $NombreCarpeta }
+
+if ($RutaCompleta.Count -gt 0) {
+    Write-Host ("Se encontró el/los directorios")
+    $RutaCompleta | ForEach-Object { Write-Host $_.FullName }
+} else {
+    Write-Host("No se ha encontrado el directorio")
+}
